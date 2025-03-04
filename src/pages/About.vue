c5b53f5254ced6ee4c74031aca64d84b47e6feed
teenage1ove <kirillstars7@gmail.com>
2025-03-04 12:23:51 +0300

    Созданы различные страницы

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Данное приложение было создано в обучающих целях</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
