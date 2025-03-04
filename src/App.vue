283fb6766ab5d838a077204ed2327f77c557c471
teenage1ove <kirillstars7@gmail.com>
2025-03-04 12:23:05 +0300

    Перенастроен компонент

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,29 @@
 <template>
-	<Navbar />
+	<navbar></navbar>
 	<div class="app">
-		<RouterView></RouterView>
+	  <router-view></router-view>
 	</div>
-</template>
-
-<script>
-import Navbar from '@/components/Navbar.vue'
-
-export default {
+  </template>
+  
+  <script>
+  import Navbar from "@/components/Navbar";
+  
+  export default {
 	components: {
-		Navbar,
-	},
-}
-</script>
-
-<style lang="scss">
-* {
+	  Navbar
+	}
+  }
+  </script>
+  
+  <style>
+  * {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
-}
-
-.app {
-	padding: 10px;
+  }
+  .app {
+	padding: 20px;
 	display: flex;
 	flex-direction: column;
-
-	&__btns {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-}
-</style>
+  }
+  </style>
